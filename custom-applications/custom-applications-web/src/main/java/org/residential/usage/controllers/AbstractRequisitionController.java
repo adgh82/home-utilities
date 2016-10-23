d8ccd43de9d3fbf0b87bc9da56abbca9852f5bfb
adgh82 <adgh130582@gmail.com>
2016-10-23 23:26:39 +0530

    created place holder

---
+++ custom-applications/custom-applications-web/src/main/java/org/residential/usage/controllers/AbstractRequisitionController.java
@@ -0,0 +1,8 @@
+package org.residential.usage.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbstractRequisitionController {
+	private static final Logger LOG = LoggerFactory.getLogger(AbstractRequisitionController.class);
+}
