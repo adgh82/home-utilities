9174aff3a70ad8773de6a6fb0616bf71a18b2b43
adgh82 <adgh130582@gmail.com>
2016-11-15 23:16:09 +0530

    config json unmarshalling

---
+++ custom-applications/custom-applications-core/src/main/java/org/residential/usage/util/Constants.java
@@ -0,0 +1,12 @@
+package org.residential.usage.util;
+
+public interface Constants {
+
+	//Strings
+	String CLASSPATH_STRING = "classpath";
+	
+	//Symbols
+	String COLON = ":";
+	String PERIOD = ".";
+	
+}
