2306a801fbbbeac1259cdf9e0ce6592432ccee62
adgh82 <adgh130582@gmail.com>
2018-06-09 00:34:11 +0530

    residual changes

---
+++ accounts-management-app/accounts-management-web/src/main/java/org/residential/usage/controllers/AbstractController.java
@@ -0,0 +1,8 @@
+package org.residential.usage.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbstractController {
+	private static final Logger LOG = LoggerFactory.getLogger(AbstractController.class);
+}
